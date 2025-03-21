64db28199dbd5ffa6db7a780efa047faa6ba5b8f
Shaine Ransford <159803141+ShaineR19@users.noreply.github.com>
2025-03-21 15:30:36 +0000

    fibo func

--- Python Docs/3.2 Fibonacci.py
+++ Python Docs/3.2 Fibonacci.py
@@ -2,7 +2,12 @@
 # 3/21/2025
 # Fibonacci
 
-a,b = 0, 1
-while a < 10000000:
-    print(a, b)
-    a, b = b, a+b
+def fib(n):
+    a,b = 0, 1
+    while a < n:
+        print(f"{a},")
+        a, b = b, a+b
+
+fib(1000)
+
+print(fib(0))
